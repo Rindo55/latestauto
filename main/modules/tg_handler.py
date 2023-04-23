c23082e74ddc2e84e3e1ef0367722ae16441edec
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-24 01:42:41 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -124,7 +124,7 @@ async def start_uploading(data):
 
         await status.edit(await status_text(f"Downloading {name}"),reply_markup=button1)
 
-        file = await downloader(msg,link,size,title)
+        file = await downloader(msg,link,title)
 
         await msg.edit(f"Download Complete : {name}")
         print("Encoding --> ",name)
